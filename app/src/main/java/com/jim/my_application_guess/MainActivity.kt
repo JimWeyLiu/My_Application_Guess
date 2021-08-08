909742dd8d7b0420fee9730865f107c36b5adb52
JimWeyLiu <amer22662747@gmail.com>
2021-08-08 15:40:52 +0800

    initial commit

---
+++ app/src/main/java/com/jim/my_application_guess/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jim.my_application_guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /** 練習
+        R.id.hello_main     //呼叫元件
+        R.drawable.eggplant //呼叫png
+        R.string.ok         //呼叫字串
+        R.color.messageColor//呼叫顏色
+        **/
+
+    }
+}
