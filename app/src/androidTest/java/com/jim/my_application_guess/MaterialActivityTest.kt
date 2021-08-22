a7bd82783af3e8cf6593303c6ada0af3eefc82c4
JimWeyLiu <amer22662747@gmail.com>
2021-08-22 17:58:25 +0800

    Nwe_MaterActivityTest

--- app/src/androidTest/java/com/jim/my_application_guess/MaterialActivityTest.kt
+++ app/src/androidTest/java/com/jim/my_application_guess/MaterialActivityTest.kt
@@ -11,28 +11,40 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)//標示這一支程式要用什麼工具來執行，類別定義
 class  MaterialActivityTest {
-    @Rule
-    @JvmField
-    val activityTestRule = ActivityTestRule<MaterialActivity>(MaterialActivity::class.java)
+    @Rule //標示符號
+    @JvmField //它代表的是這個類別，未來在Java裡面的屬性
+    val activityTestRule = ActivityTestRule<MaterialActivity>(MaterialActivity::class.java)//存取到MaterialActivity，鑽石<>符號是限定說我以後要針對什麼事情做一個Rule
 
-    @Test
+    @Test // 標示為測試方法
     fun guessWrong() {
-        val resources = activityTestRule.activity.resources
+        val resources = activityTestRule.activity.resources //經常存取簡化用，在getString使用
         val secret = activityTestRule.activity.secretNumber.secret
-//        val n = 5
-        for(n in 1..10) {
+//        val n = 5 //定義變數<-打字動作
+        for(n in 1..10) {//測試1~10
             if (n != secret) {
-//                perform進行動作
-                onView(withId(R.id.number)).perform(clearText())
-                onView(withId(R.id.number)).perform(typeText(n.toString()))
-                onView(withId(R.id.button)).perform(click())
+//                perform進行動作使用ViewActions類別.動作
+//                使用Espresso類別庫
+                onView(withId(R.id.number))//元件id(需先定義元件id)
+                    .perform(clearText())//清除字
+                onView(withId(R.id.number))//元件id(需先定義元件id)
+                    .perform(typeText(n.toString()))//打字動作
+                onView(withId(R.id.button))//元件id(需先定義元件id)
+                    .perform(click())//按一下
+//===============================================================================
+//                測試對話框
                 val message =
-                    if (n < secret) resources.getString(R.string.bigger)
-                    else resources.getString(R.string.smaller)
-                onView(withText(message)).check(matches(isDisplayed()))
-                onView(withText(resources.getString(R.string.ok))).perform(click())
+                    if (n < secret) resources.getString(R.string.bigger)//測試文字
+                    else resources.getString(R.string.smaller)//測試文字
+
+                onView(withText(message))//訊息對話框
+                    .check(matches(isDisplayed()))//驗證用，檢查有沒有出現在畫面上
+
+                onView(withText(resources
+                    .getString(R.string.ok))).perform(click())
+//===============================================================================
+//                撰寫方法：由單一測試到多重測試
             }
         }
     }
